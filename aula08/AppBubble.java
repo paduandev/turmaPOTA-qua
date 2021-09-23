a8a5ac094fa7d0ee67e0b4f5f773bcdda324388f
EmersonPaduan <emerson.paduan@gmail.com>
2021-09-22 22:01:36 -0300

    aula 08 - final

--- aula08/AppBubble.java
+++ aula08/AppBubble.java
@@ -1,9 +1,26 @@
 public class AppBubble {
     public static void main(String[] args) {
+        final int TAM_VETOR = 100;
+        int[] v = new int[TAM_VETOR];
+        int[] v2 = new int[TAM_VETOR];
         Bubble bubble = new Bubble(10);
 
+        for (int i = 0; i < v.length; i++) {
+            v[i] = (int) (Math.random() * v.length); // aleatório
+            // v[i] = i; // crescente
+            // v[i] = v.length - i; // decrescente
+            v2[i] = v[i];
+        }
+
+        bubble.setV(v);
         System.out.println(bubble.show());
-        bubble.bubbleSort();
+        // bubble.bubbleSort();
+        bubble.bubbleSortRec();
         System.out.println(bubble.show());
+        System.out.println("Comparações: " + bubble.getNumComparacoes());
+
+        // bubble.setV(v2);
+        // bubble.bubbleSortOtimizado();
+        // System.out.println("Comparações: " + bubble.getNumComparacoes());
     }
 }
--- aula08/Bubble.java
+++ aula08/Bubble.java
@@ -1,9 +1,18 @@
 public class Bubble {
     private int[] v;
+    private int numComparacoes;
 
     public Bubble(int tam) {
         v = new int[tam];
         makeRandon();
+    }
+
+    public Bubble(int[] v) {
+        this.v = v;
+    }
+
+    public void setV(int[] v) {
+        this.v = v;
     }
 
     public void makeRandon() {
@@ -12,16 +21,86 @@ public class Bubble {
         }
     }
 
-    public void bubbleSort() {
-        int iteracao, i, aux;
+    public int getNumComparacoes() {
+        return numComparacoes;
+    }
 
-        for (iteracao = 0; iteracao < v.length - 1; iteracao++) {
-            for (i = 0; i < v.length - 1 - iteracao; i++)
+    private void swap(int i, int j) {
+        int aux;
+        aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+    }
+
+    public void bubbleSort() {
+        int iteracao, i;
+
+        numComparacoes = 0;
+        for (iteracao = 0; (iteracao < v.length - 1); iteracao++) {
+            for (i = 0; i < v.length - 1 - iteracao; i++) {
+                numComparacoes++;
                 if (v[i] > v[i + 1]) {
-                    aux = v[i];
-                    v[i] = v[i + 1];
-                    v[i + 1] = aux;
+                    swap(i, i + 1);
                 }
+            }
+        }
+    }
+
+    public void bubbleSortOtimizado() {
+        int iteracao, i;
+        boolean trocou = true;
+
+        numComparacoes = 0;
+        for (iteracao = 0; (iteracao < v.length - 1) && trocou; iteracao++) {
+            trocou = false;
+            for (i = 0; i < v.length - 1 - iteracao; i++) {
+                numComparacoes++;
+                if (v[i] > v[i + 1]) {
+                    trocou = true;
+                    swap(i, i + 1);
+                }
+            }
+            // if(!trocou) break;
+        }
+    }
+
+    public void bubbleSortRec() {
+        numComparacoes = 0;
+        bubbleSortRec(v.length - 1);
+    }
+
+    private void bubbleSortRec(int end) {
+
+        for (int i = 0; i < end; i++) {
+            numComparacoes++;
+            if (v[i] > v[i + 1]) {
+                swap(i, i + 1);
+            }
+        }
+        if (end > 1) {
+            bubbleSortRec(end - 1);
+        }
+
+    }
+
+    public void bubbleSortBiDirecional() {
+        // o número de interações será dividido pela metade pois a cada passo
+        // dois números estarão no lugar (o menor e o maior)
+
+        for (int iteracao = 0; iteracao < (v.length) / 2; iteracao++) {
+            // a "iteração" determina a quantidade de valores "descartados" do início e do final
+            // este laço vai do início para o final do vetor
+            for (int i = iteracao; i < v.length - 1 - iteracao; i++) {
+                if (v[i] > v[i + 1]) {
+                    swap(i, i + 1);
+                }
+            }
+            // este laço vai do final para o início do vetor
+            for (int i = v.length - 1 - iteracao; i > iteracao; i--) {
+                if (v[i] < v[i - 1]) {
+                    swap(i, i - 1);
+                }
+            }
         }
     }
 
